0ca0ac1d12c6c21403e17e2d7a55c4426242fd92
Fredrik Larsson <epost.larsson@gmail.com>
2019-09-28 20:51:51 +0200

    skipping autoloader for classes

--- controller/appcontroller.php
+++ controller/appcontroller.php
@@ -1,10 +1,14 @@
 <?php
-function autoloader($class_name)
-{
-    include './model/' . $class_name . '.Class.php';
-}
+// function autoloader($class_name)
+// {
+//     include './model/' . $class_name . '.Class.php';
+// }
 
-spl_autoload_register('autoloader');
+// spl_autoload_register('autoloader');
+
+include './model/Member.Class.php';
+include './model/MembershipList.Class.php';
+include './model/Boat.Class.php';
 
 $form = false;
 if (isset($_REQUEST['form'])) {
